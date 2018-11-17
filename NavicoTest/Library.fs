3a4d01a558b04ee4bf72b3972dc823eb06602166
Ryan Roberts <ryansroberts@gmail.com>
2018-11-17 13:33:58 +0000

    Introduce player, allow winning

--- NavicoTest/Library.fs
+++ NavicoTest/Library.fs
@@ -4,49 +4,73 @@ open Expecto
 open SnakesAndLadders
 
 [<Tests>]
-let accept =
+let ``Feature 1.1`` =
     testList "Token can move across the board" [
         test "Starting a game" {
             //Given the game is started
             let game = Game.start()
             //When the token is placed on the board
-                           .Place(Token.create ())
+                           .Place(Player.create ())
             //Then the token is on square 1
-            Expect.equal (game.Token()) 
-                         (Some (Token.Token (uint8 1))) 
+            Expect.equal (Option.map (fun (x:Player.Player) -> x.Square) game.Player) 
+                         (Some (uint8 1)) 
                          "Token is not on square 1"
         
         }
         test "Moving a token" {
             //Given the token is on square 1
-            let token = Token.create ()
+            let player = Player.create ()
             let game = Game.start()
-                           .Place(Token.create ())
+                           .Place(player)
             //When the token is moved 3 spaces
-                           .Place(token.Move(uint8 3))
+                           .Place(player.Move(uint8 3))
             //Then the token is on square 4
-            Expect.equal (game.Token()) 
-                         (Some (Token.Token (uint8 4))) 
+            Expect.equal (Option.map (fun (x:Player.Player) -> x.Square) game.Player) 
+                         (Some (uint8 4)) 
                          "Token is not on square 4"
         
         }
         test "Moving a token again" {
             //Given the token is on square 1
-            let token = Token.create ()
+            let player = Player.create ()
             let game = Game.start()
-                           .Place(Token.create ())
+                           .Place(player)
                            .Place(
                                 //When the token is moved 3 spaces
-                                token.Move(uint8 3)
+                                player.Move(uint8 3)
                                 //And then it is moved 4 spaces
                                      .Move(uint8 4)
                            )
             //Then the token is on square 8
-            Expect.equal (game.Token()) 
-                         (Some (Token.Token (uint8 8))) 
+            Expect.equal (Option.map (fun (x:Player.Player) -> x.Square) game.Player) 
+                         (Some (uint8 8))
                          "Token is not on square 8"
          }
     ]
+
+[<Tests>]
+let ``Feature 1.2`` =
+    testList "Player can win the game" [
+        test "Winning" {
+            //Given the token is on square 97
+            let player = Player.create ()
+
+            let player = player.Move(uint8 96)
+            let game = Game.start()
+                           .Place(player)
+            //When the token is moved 3 spaces
+            let game = game.Place(player.Move(uint8 3))
+            //Then the token is on square 100
+            Expect.equal (Option.map (fun (x:Player.Player) -> x.Square) game.Player) 
+                         (Some (uint8 100)) 
+                         "Token is not on square 100"
+            Expect.equal (Option.map (fun (x:Player.Player) -> x.HasWon) game.Player)
+                    (Some (true)) 
+                    "Token is not on square 1"
+        }
+    ]    
+
+
 [<EntryPoint>]
 let runner argv = 
     Tests.runTestsInAssembly defaultConfig argv
--- NavicoTest/SnakesAndLadders.fs
+++ NavicoTest/SnakesAndLadders.fs
@@ -1,15 +1,24 @@
 ﻿module SnakesAndLadders
 
 module Token =
-    type Token = Token of uint8 with
-        member this.Move places = 
-            match this with Token x -> Token (x + places)
-            
+    type Token = Token of uint8
+                
     let create () = Token (uint8 1)
 
+module Player =
+    type Player = Player of Token.Token with
+        member this.Move places = 
+            match this with Player (Token.Token x) -> Player <| Token.Token (x + places)
+        member this.Square =
+            match this with Player (Token.Token x) -> x
+        member this.HasWon =
+            this.Square = (uint8 100)
+
+    let create () = Player <| Token.create ()
+    
 module Game =
-    type Game = Game of Token.Token option with 
-        member this.Place (token:Token.Token) = Game <| Some token
-        member this.Token () : Token.Token option = match this with Game x -> x
+    type Game = Game of Player.Player option with 
+        member this.Place (player:Player.Player) = Game <| Some player
+        member this.Player : Player.Player option = match this with Game x -> x
                
     let start () = Game None
